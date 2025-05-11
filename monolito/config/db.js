7e5a88957563c91cc8e3ca30d1322808e60fb0d0
ceciliore <doandradejr@gmail.com>
2025-05-10 23:07:09 -0300

    Add retry no banco

--- monolito/config/db.js
+++ monolito/config/db.js
@@ -1,16 +1,18 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.PSQL_NAME,
-    process.env.PSQL_USER,
-    process.env.PSQL_PASS,
-    {
-        host: process.env.PSQL_HOST,
-        port: process.env.PSQL_PORT,
-        dialect: 'postgres',
-        logging: true,
+  process.env.PSQL_NAME,
+  process.env.PSQL_USER,
+  process.env.PSQL_PASS,
+  {
+    host: process.env.PSQL_HOST,
+    port: process.env.PSQL_PORT,
+    dialect: 'postgres',
+    logging: false,
+    retry: {
+      max: 10, // tenta até 10 vezes
     }
+  }
 );
 
 module.exports = sequelize;
